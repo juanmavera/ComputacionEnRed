7dff5c94a35eecdd6b9f3da1bafa94038cb0c755
juanmavera <manuel.vera@edu.uah.es>
2018-10-19 22:40:57 +0200

    main app

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#"*** coding:"utf*8"***
+
+import numpy as np
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1> Aplicacion </h1>'
+
+
+@app.route('/loc')
+def location():
+    return'<p> UAH </p>'
+
+
+if __name__ == '__main__':
+    # app = flask(__name__)
+    app.debug = True
+    app.run(host='0.0.0.0')
