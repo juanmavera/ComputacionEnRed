09789e855504180c71fc31b3695abb9bc0f8ae74
juanmavera <manuel.vera@edu.uah.es>
2018-10-20 14:45:50 +0200

    app main

--- app.py
+++ app.py
@@ -1,5 +1,54 @@
 import numpy as np
+import re
+
 from flask import Flask
+from urllib import urlopen
+
+class webHandler():
+    def __init__(self):
+        self.web = 'https://www.meneame.net/'
+
+    def get_web(self):
+        page = urlopen(self.web).read()
+        # print(page)
+        return page
+
+    def get_data(self, page):
+
+        # Obteninendo Clics
+        patronClics = re.compile('<div class="clics">(.*?)</div>')
+        listaClics = patronClics.findall(page)
+        listaClics = listaClics[5:]
+        Clics = np.zeros(np.array(listaClics).shape[0])
+
+        print('Clics obtenidos')
+
+        for index, lista in enumerate(listaClics):
+            aux = str(lista).split(' ')
+            Clics[index] = int(aux[2])
+            print(index, Clics[index])
+
+        # Obteniendo Meneos
+        patronMeneos = re.compile('<div class="votes"> <a (.*?)>(.*?)</a> meneos </div>')
+        listaMeneos = patronMeneos.findall(page)
+        Meneos = np.zeros(np.array(listaMeneos).shape[0])
+
+        print('Meneos Obtenidos')
+
+        for index, lista in enumerate(listaMeneos):
+            Meneos[index] = int(lista[1])
+            print(index, Meneos[index])
+
+        # Obteniendo Titulos
+        patronNews = re.compile('<h2> <a href=(.*?) > (.*?) </a>(.*?)</h2>')
+        listaNews = patronNews.findall(page)
+        News = np.array(listaNews)[:, 1]
+
+        print('Titulos Obtenidos')
+
+        for index, lista in enumerate(listaNews):
+            print(index, str(News[index]))
+
 
 app = Flask(__name__)
 
@@ -14,6 +63,10 @@ def location():
 
 
 if __name__ == '__main__':
-    # app = flask(__name__)
-    app.debug = True
-    app.run(host='0.0.0.0')
+
+    WWW = webHandler()
+    page = WWW.get_web()
+    WWW.get_data(page)
+
+    # app.debug = True
+    # app.run(host='0.0.0.0')
