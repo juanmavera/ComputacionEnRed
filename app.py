4753de045c6b03c6e6fc2887fbf49c6b1b4f4786
juanmavera <manuel.vera@edu.uah.es>
2018-10-20 16:50:43 +0200

    app main

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> Aplicacion </h1>'
+    Data = DataObtainer()
+    page = Data.get_web()
+    Noticia = Data.get_web_data(page)
+
+    cadena = "Clicks: %d || Meneos: %d || Noticia: %s || Fecha: %s || Hora: %s" % (int(float(Noticia[0])), int(float(Noticia[1])), str(Noticia[2]), str(Noticia[3]), str(Noticia[4]))
+    return '<p> %s </p>' %(cadena)
 
 
 @app.route('/loc')
@@ -16,10 +21,12 @@ def location():
 
 if __name__ == '__main__':
 
+
     Data = DataObtainer()
     page = Data.get_web()
     Noticia = Data.get_web_data(page)
 
-    print("Clicks: %d || Meneos: %d || Noticia: %s" % (int(float(Noticia[0])), int(float(Noticia[1])), str(Noticia[2])))
+    print("Clicks: %d || Meneos: %d || Noticia: %s || Fecha: %s || Hora: %s" % (int(float(Noticia[0])), int(float(Noticia[1])), str(Noticia[2]), str(Noticia[3]), str(Noticia[4])))
+
     # app.debug = True
     # app.run(host='0.0.0.0')
