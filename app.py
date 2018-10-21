9ddc7935889c148df192b443cabf6029be874fd9
juanmavera <manuel.vera@edu.uah.es>
2018-10-21 21:51:03 +0200

    app main

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def CalculaMedia(Mongo=True):
     else:
         bbtDB = BeebotteHandler()
         Noticias = bbtDB.LeerNoticias()
-        Noticias = list(reversed(Noticias))
 
     mediaClics = np.mean(Noticias[0])
     mediaMeneos = np.mean(Noticias[1])
