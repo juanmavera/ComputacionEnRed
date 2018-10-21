84861e3ec063995b43e5aa19f230055bfe23d206
juanmavera <manuel.vera@edu.uah.es>
2018-10-21 16:23:18 +0200

    app main

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from DataObtainer import *
-
+import threading
+import time
 
 app = Flask(__name__)
+
+
+def InitPeriodicDataObtainer():
+    Data = DataObtainer()
+    page = Data.get_web()
+    Noticia = Data.get_web_data(page)
+
+    print("Clicks: %d || Meneos: %d || Noticia: %s || Fecha: %s || Hora: %s" % (int(float(Noticia[0])), int(float(Noticia[1])), str(Noticia[2]), str(Noticia[3]), str(Noticia[4])))
+
+    threading.Timer(120, InitPeriodicDataObtainer).start()
+
 
 @app.route('/')
 def index():
@@ -21,12 +33,7 @@ def location():
 
 if __name__ == '__main__':
 
-
-    Data = DataObtainer()
-    page = Data.get_web()
-    Noticia = Data.get_web_data(page)
-
-    print("Clicks: %d || Meneos: %d || Noticia: %s || Fecha: %s || Hora: %s" % (int(float(Noticia[0])), int(float(Noticia[1])), str(Noticia[2]), str(Noticia[3]), str(Noticia[4])))
+    InitPeriodicDataObtainer()
 
     # app.debug = True
     # app.run(host='0.0.0.0')
